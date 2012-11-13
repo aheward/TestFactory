00e7d2b1a645401a2a9e37f87bd61b642c9976d9
Abraham Heward <aheward@rsmart.com>
2012-11-13 12:57:15 -0700

    Additional comments on code. Bump to new gem version.

--- lib/test-factory/gem_ext.rb
+++ lib/test-factory/gem_ext.rb
@@ -1,6 +1,10 @@
 module Watir
   module UserEditable
 
+    # Extends Watir's methods.
+    # Use when the argument you are passing to a text field
+    # may be nil, in which case you don't
+    # want to do anything with the page element.
     def fit(*args)
       unless args==nil
         assert_exists
@@ -13,6 +17,10 @@ module Watir
   end
 
   class Select
+    # Extends Watir's methods.
+    # Use when the argument you are passing to a text field
+    # may be nil, in which case you don't
+    # want to do anything with the page element.
     def fit(str_or_rx)
       select_by :text, str_or_rx unless str_or_rx==nil
     end
--- lib/test-factory/page_factory.rb
+++ lib/test-factory/page_factory.rb
@@ -13,18 +13,25 @@ class PageFactory
 
   class << self
 
+    # Define this in a page class and when you use the "visit" method to instantiate the class
+    # it will enter the URL the browser's address bar.
     def page_url url
       define_method 'goto' do
         @browser.goto url
       end
     end
 
+    # Define this in a page class and when that class is instantiated it will wait until that
+    # element appears on the page before continuing with the script.
     def expected_element element_name, timeout=30
       define_method 'expected_element' do
         self.send(element_name).wait_until_present timeout
       end
     end
 
+    # Define this in a page class and when the class is instantiated it will verify that
+    # the browser's title matches the expected title. If there isn't a match, it raises an
+    # error and halts the script.
     def expected_title expected_title
       define_method 'has_expected_title?' do
         has_expected_title = expected_title.kind_of?(Regexp) ? expected_title =~ @browser.title : expected_title == @browser.title
@@ -32,6 +39,8 @@ class PageFactory
       end
     end
 
+    # The basic building block of the page object classes.
+    # Use in conjunction with Watir to define all elements on a given page that are important to validate.
     def element element_name
       raise "#{element_name} is being defined twice in #{self}!" if self.instance_methods.include?(element_name.to_sym)
       define_method element_name.to_s do
@@ -39,14 +48,13 @@ class PageFactory
       end
     end
     alias :value :element
-    alias :thing :element
 
+    # The basic building block for interacting with elements on a page, such as links and buttons.
     def action method_name, &block
       define_method method_name.to_s do |*thing|
         Proc.new(&block).call *thing, self
       end
     end
-    alias :pgmd :action
 
   end
 
