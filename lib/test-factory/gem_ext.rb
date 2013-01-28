d989d160b8ed4490c0ab659d6171988468888c95
Abraham Heward <aheward@rsmart.com>
2013-01-28 08:38:47 -0700

    Added comments about proper usage of the #fit method. Bumped the gem version number.

--- lib/test-factory/gem_ext.rb
+++ lib/test-factory/gem_ext.rb
@@ -1,3 +1,31 @@
+# We are extending Watir's element methods here with the #fit method,
+# which can be used with text fields, select lists, radio buttons,
+# and checkboxes.
+#
+# The purpose of #fit is to allow the creation, in your Data Object classes,
+# of a minimal number of #edit methods (ideally only one) written as
+# concisely as possible.
+#
+# Without the #fit method, you would either have to write separate edit
+# methods for every possible field you want to edit, or else your
+# edit method would have to contain lots of repetitive conditional code
+# to prevent making edits to those fields that don't need it.
+#
+# Proper use of the #fit method requires following a particular coding
+# pattern, however:
+#
+# 1) In your Page Classes, define your radio buttons and checkboxes
+#    directly. Do not define #set and/or #clear actions there.
+# 2) Your data object's instance variables for radio buttons and
+#    checkboxes should have the values of :set or :clear. If they NEED to be
+#    something else, then define a Hash transform method to easily
+#    convert the custom values back to :set or :clear, then pass that
+#    transform to the #fit method.
+# 3) Always remember to end your #edit methods with the #set_options()
+#    method, from the DataFactory module. It automatically takes care
+#    of updating your data object's instance variables with any
+#    new values.
+#
 module Watir
   module UserEditable
 
