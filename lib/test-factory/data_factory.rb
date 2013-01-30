dbf229813be2e39738a1118009965ff965ce624e
Abraham Heward <aheward@rsmart.com>
2013-01-30 07:20:24 -0700

    Added the #get_or_select! method. Fixed a problem with #get_or_select to ensure that it returns the proper value back to the instance variable.

--- lib/test-factory/data_factory.rb
+++ lib/test-factory/data_factory.rb
@@ -39,22 +39,44 @@ module DataFactory
 
   # Use this method in your data object class for select list boxes that will have
   # default values you may be interested in. The method will either set the select
-  # box to the value specified, or, if no value exists for the given variable in your
+  # box to the value specified, or, if no value exists for the given instance variable in your
   # data object, it will get the value from the UI and set the data object's
   # variable to that value. Note that this only supports select lists that allow
   # a single selection.
-  # @param var [instance variable] The instance variable that will either be set or be used to update the page
+  # @param inst_var_sym [Symbol] A Symbol that _must_ match the instance variable that will either be set or be used to update the page
   # @param select_list [Watir::Select] The relevant select list element on the page
   #
   # @example
   #
-  #   @num_resubmissions = get_or_set(@num_resubmissions, page.num_resubmissions)
-  def self.get_or_select!(inst_var_sym)
+  #   get_or_select! :@num_resubmissions, page.num_resubmissions
+  def get_or_select!(inst_var_sym, select_list)
     value = instance_variable_get inst_var_sym
     if value==nil
-      instance_variable_set inst_var_sym, self.selected_options[0].text
+      instance_variable_set inst_var_sym, select_list.selected_options[0].text
     else
-      self.select value
+      select_list.select value
+    end
+  end
+
+  # This method accomplishes the same thing as #get_or_select! but
+  # is used specifically when the instance variable being used/updated
+  # is a Hash. Pay close attention to the syntax differences between
+  # this method and @get_or_select!
+  #
+  # First, note that the returned value of this method must be
+  # passed to the relevant key in the Hash. Note also that unlike
+  # #get_or_select!, this method does not take a symbol representation
+  # of the instance variable.
+  #
+  # @example
+  #
+  #   @open[:day] = get_or_select(@open[:day], page.open_day)
+  def get_or_select(hash_inst_var, select_list)
+    if hash_inst_var==nil
+      select_list.selected_options[0].text
+    else
+      select_list.select hash_inst_var
+      hash_inst_var
     end
   end
 
