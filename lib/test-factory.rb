681434e45903b2b269e8311d9769172f5c3dd698
Abraham Heward <aheward@rsmart.com>
2013-11-27 12:34:30 -0700

    Updated gem to load files in a particular order to ensure that the data factory module would be available to the data object superclass.

--- lib/test-factory.rb
+++ lib/test-factory.rb
@@ -13,5 +13,5 @@
 # limitations under the License.
 
 require 'watir-webdriver'
-require "#{File.dirname(__FILE__)}/test-factory/foundry.rb"
+%w{foundry.rb data_factory.rb}.each { |f| require "#{File.dirname(__FILE__)}/test-factory/#{f}" }
 Dir["#{File.dirname(__FILE__)}/test-factory/*.rb"].each {|f| require f }
